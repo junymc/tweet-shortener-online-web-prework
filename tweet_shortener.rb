ff27c2889fccc436c460545f755bd32a21e5b0c0
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-21 20:48:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,14 @@
+dictionary = {
+               "hello" => 'hi',
+               ["to", "two", "too"] => '2',
+               ["for", "four"] => '4',
+               "be" => 'b',
+               "you" => 'u',
+               "at" => '@',
+               "and" => '&'
+             }
+
+
 def word_substituter(string)
   string.split
   string.each do |word|
