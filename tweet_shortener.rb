ba1eb60255ac829f4a2d1817d2bbd10dbf6e1321
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-22 02:23:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,7 +53,7 @@ end
 def shortened_tweet_truncator(tweet)
   word_substituter(tweet)
   if word_substituter(tweet).size > 140
-     word_substituter(tweet)[0..139] << "..."
+     word_substituter(tweet)[0..136] << "..."
   else
     tweet
   end
